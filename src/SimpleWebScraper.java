25649d9d97a425611382086bc66a093e03880684
andrew <51986515+andrewtmn@users.noreply.github.com>
2019-10-28 01:59:42 +1000

    fetched hyper links and page title from a web page

--- src/SimpleWebScraper.java
+++ src/SimpleWebScraper.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -37,9 +39,30 @@ public class SimpleWebScraper {
         Document doc2 = Jsoup.parse(tmp.toString());
     }
 
+    public static void scrapeHyperLinks() throws IOException {
+        // fetch the document over HTTP
+        Document doc = Jsoup.connect("https://www.amazon.com/NPKC-"
+                + "Customized-Suitable-Switches-Mechanical/dp/B06XWWRRXT/"
+                + "ref=sr_1_8?keywords=keyboard+caps&qid=1572181759&sr=8-8")
+                .get();
+
+        // get the page title
+        String title = doc.title();
+        System.out.println(title);
+
+        // get all links in page
+        Elements links = doc.select("a[href]");
+        for (Element link : links) {
+            //get the value from the href attribute
+            System.out.println("\nlink: " + link.attr("href"));
+            System.out.println("text: " + link.text());
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            pageFetcher();
+//            pageFetcher();
+            scrapeHyperLinks();
         } catch (IOException ioe) {
             System.out.println("something went wrong");
         }
