a25e3f9f4b17885c446d410c641a6418977f24d8
andrew <51986515+andrewtmn@users.noreply.github.com>
2019-11-06 12:02:09 +1000

    Added comments. Did some abstracting out of the EmailSender class so that the Strings are instantiated in AmazonScraper class.
    Will abstract this out into a GUI later on
    
    Employ a MVC model - The view is the basic gui window where you can input
    the url, e-mails and password
    
    The model is the actual logic - i.e. extract the price and send the email
    
    The controller will process the input from the view and communicate with
    the model

--- src/AmazonScraper.java
+++ src/AmazonScraper.java
@@ -9,7 +9,17 @@ import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * Basic application that gets the product buy price from a product page
+ * and sends an e-mail if the price is below a threshold
+ */
 public class AmazonScraper {
+
+    /**
+     *
+     * @param URL the url of the web page of the product on Amazon
+     * @throws IOException when an error occurs trying to connect to the resource
+     */
     public static void scrapeBuyPrice(String URL) throws IOException {
         Document doc = Jsoup.connect(URL).get();
 
@@ -20,17 +30,36 @@ public class AmazonScraper {
         Elements price = doc.select("#price_inside_buybox");
         System.out.println("The current price is " + price.html());
 
-        checkIfBuyable(price.html());
+        if (checkIfBuyable(price.html())) {
+            sendNotification();
+        }
     }
 
-    private static void checkIfBuyable(String price) {
+    // Checks if the price of the product is under a chosen threshold
+    private static boolean checkIfBuyable(String price) {
         // remove the dollar sign so price can be parsed
         price = price.substring(1);
         double parsedPrice = Double.parseDouble(price);
 
-        if (parsedPrice < 10000) {
-           EmailSender.sendEmail();
-        }
+        return (parsedPrice < 10000);
+    }
+
+    // sends the e-mail to the recipient
+    private static void sendNotification() {
+        String subject = "Amazon product price drop";
+        String textContent = "You can now afford the item. The link's here ";
+        String url = "https://www.amazon.com.au/Lenovo-"
+                + "Chromebook-MediaTek-Processor-81JW0000US/dp/B07GLV1VC7?ref_"
+                + "=Oct_RAsinC_Ajax_4913311051_1&pf_rd_r=H4YWAEH6RM4J8CTWCFRN"
+                + "&pf_rd_p=c3b9c102-0e2a-5d82-b3be-581a05a49424&pf_rd_s=merc"
+                + "handised-search-10&pf_rd_t=101&pf_rd_i=4913311051&pf_rd_"
+                + "m=ANEGB3WVEVKZB";
+        String sender = "andrewnguyenn229@gmail.com";
+        String recipient = "andrewnguyenn229@gmail.com";
+        String password = "";
+
+        EmailSender.sendEmail(subject, textContent + url,
+                sender, recipient, password);
     }
 
     public static void main(String[] args) {
@@ -47,38 +76,50 @@ public class AmazonScraper {
     }
 }
 
+/**
+ * Sends e-mail to a chosen recipient, from a given sender. Uses the Gmail
+ * server.
+ */
 class EmailSender {
 
-    public static void sendEmail() {
-
-        String from = "andrewnguyenn229@gmail.com";
-        String to = "andrewnguyenn229@gmail.com";
-        String pass = "";
-
+    /**
+     * Sends an e-mail with the given subject and text content. E-mail is sent
+     * from the given sender to the given recipient. The Gmail server is used
+     * so password authentication is required.
+     * @param subject subject of the email
+     * @param textContent text content of the email
+     * @param sender the sender of the email
+     * @param recipient the recipient of the email
+     * @param password the authentication password for the sender's email
+     */
+    public static void sendEmail(String subject, String textContent,
+                                 String sender, String recipient,
+                                 String password) {
         Properties properties = createProperties();
 
         // create a session
         Session session = Session.getDefaultInstance(properties,
                 new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, pass);
+                return new PasswordAuthentication(sender, password);
             }
         });
 
         try {
 
             Transport transport = session.getTransport();
-            InternetAddress addressFrom = new InternetAddress(from);
+            InternetAddress addressFrom = new InternetAddress(sender);
 
             MimeMessage message = new MimeMessage(session);
 
             // set the e-mail sender and recipient
             message.setSender(addressFrom);
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(recipient));
 
             // set the e-mail subject and text content
-            message.setSubject("The price on Amazon has dropped");
-            message.setContent("Check out the item, its price has been reduced", "text/plain");
+            message.setSubject(subject);
+            message.setContent(textContent, "text/plain");
 
             transport.connect();
             Transport.send(message);
@@ -93,6 +134,7 @@ class EmailSender {
 
     }
 
+    // Sets the properties of the session for the e-mail send
     private static Properties createProperties() {
         Properties properties = new Properties();
 
